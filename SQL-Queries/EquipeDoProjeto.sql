6491467ba1c366e0531d3058970910de375bec1d
Thiago Bottoni <bottonithiago@gmail.com>
2016-04-26 17:23:08 -0300

    Create EquipeDoProjeto.sql

---
+++ SQL-Queries/EquipeDoProjeto.sql
@@ -0,0 +1,17 @@
+--Retorna a equipe do investimento
+
+SELECT DISTINCT INV.NAME "Investment Name",
+		SRM.FULL_NAME "Resource",
+          	(
+          		SELECT	PROLE.LAST_NAME
+          		FROM	SRM_RESOURCES PROLE
+          		WHERE	PROLE.ID = TEAM.PRROLEID
+          	) "Project Role",
+		DECODE	(TEAM.PRBOOKING, 15, 'Hard', 10, 'Mixed', 5, 'Soft') "Booking Status",
+		DECODE	(TEAM.PRISOPEN, 1, 'False', 0, 'True') "Open for Time"
+FROM		INV_INVESTMENTS INV
+	INNER JOIN	PRTEAM TEAM
+		ON	TEAM.PRPROJECTID = INV.ID
+	INNER JOIN	SRM_RESOURCES SRM
+		ON	SRM.ID = TEAM.PRRESOURCEID
+WHERE	INV.CODE = 'PR1002' --Altere pelo código do seu projeto
