e82b183e46d6e8c8493892bfac445e262eb02537
Thiago Bottoni <bottonithiago@gmail.com>
2019-07-01 17:42:54 -0300

    Update IdeaByCustomer.sql

--- NSQL-Queries/IdeaByCustomer.sql
+++ NSQL-Queries/IdeaByCustomer.sql
@@ -1,11 +1,11 @@
---Consulta para ser usada em portlets gráficos no CA Clarity PPM. Consiste na contagem de demandas agrupadas por cliente (objeto não nativo) e com alguns filtros que podem ser usados tanto em portlets de filtro por página quanto em filtros do próprio portlet gráfico.
+--Query to be used in a graph portlet on Clarity PPM. It counts ideas grouped by customer (Custom object) and implements some useful filters.
 
 SELECT	@SELECT:DIM:USER_DEF:IMPLIED:IDEA:demandas.cliente:cliente@,
 	@SELECT:METRIC:USER_DEF:IMPLIED:COUNT(demandas.idea_id):idea_count:AGG@
 
 FROM	(
-                --Cliente não é um objeto nativo
-		SELECT 	    cli.name cliente
+                --Cliente is a custom object
+		SELECT 	    cli.name customer
 			    , inv.code idea_id
 		FROM        inv_ideas idea
 		INNER JOIN  inv_investments inv
@@ -14,22 +14,22 @@ FROM	(
 			ON  inv.id = odfi.id
 		INNER JOIN  ODF_CA_TBG_CLIENTES cli
 			ON  odfi.tbg_cliente = cli.code
-				--Filtro por status (atributo não nativo)
+				--Filter by Status (Custom attribute)
 		WHERE		(	odfi.tbg_status = 	@WHERE:PARAM:USER_DEF:STRING:status@
 					OR     			@WHERE:PARAM:USER_DEF:STRING:status@ is null
 				)
 				AND
-				--Filtro por categoria (atributo não nativo)
+				--Filter by Category (Custom attribute)
 				(	odfi.tbg_categoria = 	@WHERE:PARAM:USER_DEF:STRING:categoria@
 					OR    			@WHERE:PARAM:USER_DEF:STRING:categoria@ is null
 				)
 				AND
-				--Filtro por tipo (atributo não nativo)
+				--Filter by Type (Custom attribute)
 				(	odfi.tbg_tipo = 	@WHERE:PARAM:USER_DEF:STRING:tipo@
 					OR			@WHERE:PARAM:USER_DEF:STRING:tipo@ is null
 				)
 				AND
-				--Filtro por cliente (atributo não nativo)
+				--Filter by Customer (Custom attribute)
 				(	odfi.tbg_cliente = 	@WHERE:PARAM:USER_DEF:STRING:cliente@
 					OR    			@WHERE:PARAM:USER_DEF:STRING:cliente@ is null
 				)
