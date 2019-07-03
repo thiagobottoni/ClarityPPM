adf3b5d9d210ac286018c7bea5b1f92f08c8daf3
Thiago Bottoni <bottonithiago@gmail.com>
2019-07-03 18:45:14 -0300

    Create Select ETC and EAC DWH.sql

---
+++ SQL-Queries/Select ETC and EAC DWH.sql
@@ -0,0 +1,14 @@
+--Returns the ETC and EAC stored on the Clarity Data Warehouse
+
+SELECT      inv.investment_key         "ID", 
+            inv.investment_name        "PROJECT NAME", 
+            isf.etc_labor_hours        "ETC", 
+            isf.eac_labor_hours        "EAC"
+FROM        dwh_inv_summary_facts isf 
+INNER JOIN  dwh_inv_investment inv 
+        ON  inv.investment_key = isf.investment_key 
+WHERE       inv.investment_type_key = 'project'
+        AND inv.is_active = 1
+        AND inv.is_template = 0
+--      AND inv.investment_name = 'Project Name' --Use that line to filter by Project Name
+--		AND inv.investment_key = 5002003 --Use that line to filter by Project ID
